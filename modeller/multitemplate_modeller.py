dafdb591674e4a854e795ab2653c4ea7b3f30e68
Adam Bellaïche <adam.bellaiche@gmail.com>
2018-12-05 19:14:09 +0100

    oh la ça fait beaucoup

--- modeller/multitemplate_modeller.py
+++ modeller/multitemplate_modeller.py
@@ -25,7 +25,7 @@ env.io.atom_files_directory = ['.', './data/structures/inward_open']
 
 
 a = automodel(env,
-              alnfile  = './data/sequences/inward_open/inward_align.pir', # alignment filename
+              alnfile  = './data/structures/inward_open/inward_open.pir', # alignment filename
               knowns   = ('4LDS','4JA4', '5EQI','4PYP'),     # codes of the templates ,
               sequence = 'NPT4_in')               # code of the target
 a.starting_model= 1                 # index of the first model
@@ -48,7 +48,7 @@ env2.io.atom_files_directory = ['.', './data/structures/outward_open']
 
 
 b = automodel(env2,
-              alnfile  = './data/sequences/outward_open/outward_align.pir', # alignment filename
+              alnfile  = './data/structures/outward_open/outward.pir', # alignment filename
               knowns   = ('4YBQ', '4ZWC', '5C65'),     # codes of the templates ,
               sequence = 'NPT4_out')               # code of the target
 b.starting_model= 1                 # index of the first model
